85dc572cad75eeb6d220d74283b572105ecc595a
Octal <59418903+oliver-makes-code@users.noreply.github.com>
2022-12-17 21:18:40 -0600

    awoo

--- src/main/kotlin/dev/proxyfox/pluralkt/Response.kt
+++ src/main/kotlin/dev/proxyfox/pluralkt/Response.kt
@@ -4,8 +4,11 @@ import dev.proxyfox.pluralkt.types.*
 
 interface Response<T : ApiType> {
     fun isSuccess(): Boolean
+
     fun getSuccess(): T
+
     fun isError(): Boolean
+
     fun getError(): ApiError
 }
 
@@ -17,11 +20,11 @@ class ResponseSuccess<T : ApiType>(private val value: T) : Response<T> {
     override fun isError(): Boolean = false
 
     override fun getError(): ApiError = throw IllegalStateException("Response is not an error")
-
 }
 
 class ResponseError<T : ApiType>(private val error: ApiError) : Response<T> {
     override fun isSuccess(): Boolean = true
+
     override fun getSuccess(): T = throw IllegalStateException("Response is not a success")
 
     override fun isError(): Boolean = true
